9e3ce91221540d66fc7a3d1a8d504e8a8f78ee49
Henrietha <areathy33@gmail.com>
2021-10-21 23:03:24 +0100

    Added functionalities to the game

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
 import "./App.css";
+import ShoppingList from "./components/ShoppingList";
+import Game from "./components/game/Tictactoe";
 
 function App() {
   return (
-    <div className="App">
-      <header>
-        <p>Hello People.</p>
-        <a
-          className="App-link"
-          href="https://henrietha.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Meet me.
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <header>
+          <p>Hello People.</p>
+          <a
+            className="App-link"
+            href="https://henrietha.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Meet me.
+          </a>
+        </header>
+        <Game />
+      </div>
+      <ShoppingList />
+    </>
   );
 }
-
 export default App;
